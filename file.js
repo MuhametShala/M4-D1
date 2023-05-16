7da23da57afa5a32eb1321b541af5d5422947dff
Muhamet Shala <metshala95@gmail.com>
2023-05-16 20:14:36 +0200

    first commit
    hello

---
+++ file.js
@@ -0,0 +1,27 @@
+
+
+/* function numberControl(numeroUno, numeroDue){
+    if (numeroUno===50 || numeroDue===50 || (numeroUno+numeroDue)===50){
+        console.log("true");
+    }
+
+    else {
+        console.log("false")
+    }
+    
+}
+
+numberControl(13,36); */
+
+let stringa = [Epicode];
+
+for (let i = 0; i < epicode.length; i++) {
+    if(stringa.include("o")){
+        console.log(stringa.splice("o"))
+    }
+
+    else{
+        console.log("non ce stà!")
+    }
+};
+
